992ad81d9ae7b9d0370bb4ca3d251f20f52bec18
Débora <debora.d.r.a@usp.br>
2021-07-01 01:33:48 -0300

    scripts

---
+++ init_pgadmin.sh
@@ -0,0 +1,2 @@
+source pgadmin4/bin/activate
+pgadmin4 & firefox 127.0.0.1:5050
---
+++ system_info.sh
@@ -0,0 +1,1 @@
+lsb_release -d && cat /proc/cpuinfo | grep model name | head -1 && cat /proc/meminfo | grep MemTotal | head -1
