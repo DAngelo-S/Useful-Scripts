eb9cefd6c94344c60cc9440924cc00ddfab99de4
Débora <debora.d.r.a@usp.br>
2021-07-26 18:19:30 -0300

    fixed error on model name of CPU

--- system_info.sh
+++ system_info.sh
@@ -1,1 +1,1 @@
-lsb_release -d && cat /proc/cpuinfo | grep model name | head -1 && cat /proc/meminfo | grep MemTotal | head -1
+lsb_release -d && cat /proc/cpuinfo | grep "model name" | head -1 && cat /proc/meminfo | grep "MemTotal" | head -1
